09370208444224ef36704a966eae5a2ea168d0eb
sanghyuck kim <sanghyuck kim>
2017-01-03 12:16:00 +0900

    js연습

--- js_ex.js
+++ js_ex.js
@@ -1,4 +1,32 @@
-var m=10;
-var m='a';
-document.write(m>>m);
-document.write(typeof(m>>m));
+//100부터 1까지 거꾸로 찍기
+
+for(i=100;i>0;i--){
+    console.log(i);
+}
+
+/*
+*
+**
+***
+****
+*****
+별모양 만들기
+
+for(var i=0;i<5;i++){
+   if(i=1){console.log('*'+'/br');}
+}
+
+*/
+  var result ='';
+  for(var i=0;i<5;i++){ 
+   result +='*'      
+   console.log(result); 
+}
+
+// 반대는 어떻게?
+ var result ='*';
+  for(var i=0;i<5;i++){ 
+   result =>'*'      
+   console.log(result); 
+}
+   
